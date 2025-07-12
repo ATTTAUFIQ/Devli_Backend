c56896e43e55d03b30205fef5009b80aebb50ab2
Taufiq Attar <workattartaufiq@gmail.com>
2025-07-12 17:08:06 +0530

    routing

--- src/app.js
+++ src/app.js
@@ -1,6 +1,22 @@
 import express from "express";
 const app = express();
 
+const data = {
+  name: "Attar Taufiq",
+  age: 22,
+  city: "Pune",
+};
+
+app.get("/user-details", (req, res) => {
+  res.send(data);
+});
+
+app.post("/user-details", (req, res) => {
+  res.send("Posted");
+});
+app.use("/test/info", (req, res) => {
+  res.send("IN test Info");
+});
 app.use("/test", (req, res) => {
   res.send("Testing Success");
 });
