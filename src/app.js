309ceb629b1466bc6ca0287e935f1a3cf4c62978
Taufiq Attar <workattartaufiq@gmail.com>
2025-07-12 15:17:36 +0530

    created a server

--- src/app.js
+++ src/app.js
@@ -1,6 +1,13 @@
-import express from "express"
-const app=express();
-app.listen(8080,()=>
-{
-    console.log("Server Running on Port 8080")
-})
+import express from "express";
+const app = express();
+
+app.use("/test", (req, res) => {
+  res.send("Testing Success");
+});
+
+app.use((req, res) => {
+  res.send("Hello To server");
+});
+app.listen(8080, () => {
+  console.log("Server Running on Port 8080");
+});
