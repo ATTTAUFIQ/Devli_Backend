86941411f0c95088e982ca061bc3588680d3cfb7
Taufiq Attar <workattartaufiq@gmail.com>
2025-07-12 22:44:59 +0530

    middleware

--- src/app.js
+++ src/app.js
@@ -1,22 +1,20 @@
 import express from "express";
 const app = express();
 
+app.use(
+  "/user/:id",
+  (req, res, next) => {
+    next();
+  },
+  (req, res, next) => {
+  res.send("I am under water")
+  next()
+  },
+  (req, res, next) => {
+    res.send("I am under water")
+  }
+);
 
-
-app.get("/user/:id",(req,res)=>
-{
-  res.send(req.params)
-})
-app.get("/user",(req,res)=>
-{
-res.send(req.query.usedid)
-})
-
-
-
-app.use("/",(req, res) => {
-  res.send("Hello To server");
-});
 app.listen(8080, () => {
   console.log("Server Running on Port 8080");
 });
