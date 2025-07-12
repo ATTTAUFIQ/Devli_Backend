41cb82f2bedf3043407fa3042ac92941572ac758
Taufiq Attar <workattartaufiq@gmail.com>
2025-07-12 17:18:55 +0530

    advance routing

--- src/app.js
+++ src/app.js
@@ -1,27 +1,20 @@
 import express from "express";
 const app = express();
 
-const data = {
-  name: "Attar Taufiq",
-  age: 22,
-  city: "Pune",
-};
 
-app.get("/user-details", (req, res) => {
-  res.send(data);
-});
 
-app.post("/user-details", (req, res) => {
-  res.send("Posted");
-});
-app.use("/test/info", (req, res) => {
-  res.send("IN test Info");
-});
-app.use("/test", (req, res) => {
-  res.send("Testing Success");
-});
+app.get("/user/:id",(req,res)=>
+{
+  res.send(req.params)
+})
+app.get("/user",(req,res)=>
+{
+res.send(req.query.usedid)
+})
 
-app.use((req, res) => {
+
+
+app.use("/",(req, res) => {
   res.send("Hello To server");
 });
 app.listen(8080, () => {
